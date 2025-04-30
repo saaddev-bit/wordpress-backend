6dbd5227f68adc3485498524f7348ea3ee2bd60f
DESKTOP-KK186JQ\Saad <saad.waheed@devsspace.com>
2025-04-30 11:55:24 +0500

    Add docblocks to plugins

--- wp-content/plugins/my-first-plugin/my-first-plugin.php
+++ wp-content/plugins/my-first-plugin/my-first-plugin.php
@@ -6,23 +6,29 @@ Version: 1.0
 Author: Saad Waheed
 */
 
-//Activation Hook
+/** 
+ *Activation Hook 
+*/
 function mfp_activate() {
     update_option( 'mfp_version', '1.0');
     error_log( 'My First Plugin activated' );
 }
 register_activation_hook( __FILE__, 'mfp_activate' );
 
-
+/**
+ * Shortcode to display a hello message.
+ *
+ * @return string The hello message.
+ */
 function mfp_shortcode() {
     return 'Hello, this is my first plugin!';
 }
 add_shortcode( 'mfp_hello', 'mfp_shortcode' );
 
 
-
-
-//Deactivation Hook
+/**
+ * Deactivation Hook
+ */
 function mfp_deactivate() {
     error_log( 'My First Plugin deactivated' );
 }
